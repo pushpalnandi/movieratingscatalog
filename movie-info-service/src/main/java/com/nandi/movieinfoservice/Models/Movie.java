a16dd60098b5c2a5ffa3a6f7a655ece2c621e869
pushpalnandi <74118161+pushpalnandi@users.noreply.github.com>
2020-11-08 03:23:48 +0530

    Add files via upload

---
+++ movie-info-service/src/main/java/com/nandi/movieinfoservice/Models/Movie.java
@@ -0,0 +1,30 @@
+package com.nandi.movieinfoservice.Models;
+
+public class Movie {
+
+    public Movie(String movieId, String name) {
+        this.movieId = movieId;
+        this.name = name;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String movieId;
+    private String name;
+
+
+}
